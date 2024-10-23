f17e1c0fde74ef8043e8baf17bafe9cfc53b80da
Muhamed ibrahim <92036859+Mohammadibbu@users.noreply.github.com>
2024-10-23 18:07:12 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -142,6 +142,20 @@ function App() {
 
   return (
     <div className="container">
+      <div class="area">
+        <ul class="circles">
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
       <div className="content">
         <h3>Password Strength Checker</h3>
 
